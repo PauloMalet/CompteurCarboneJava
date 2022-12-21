fc7d472bf906e32a85655e0b9add4dfc0f4508da
Barbea <113396124+Barbea@users.noreply.github.com>
2022-12-21 11:06:27 +0000

    Update CE.java

--- ProjetJava/src/consoCarbone/CE.java
+++ ProjetJava/src/consoCarbone/CE.java
@@ -1,4 +1,7 @@
 package consoCarbone;
+/** 
+ * Cette enumeration représente les différentes classes énergétiques possibles d’un logement.
+ */
 
 public enum CE {
 	A(0.005), B(0.01), C(0.02), D(0.035), E(0.055), F(0.08), G(0.1);
@@ -9,6 +12,10 @@ public enum CE {
 		this.impactCE = impactCE;
 	}
 	
+	/**
+	 * 
+	 * @return l'impact en termes d’émissions en tonne de GES en TCO2eq de la classe énergétique 
+	 */
 	public double getImpactCE() {
 		return impactCE;
 	}
