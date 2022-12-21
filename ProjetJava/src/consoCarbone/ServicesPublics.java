ffc4f82816ba0cff14f0b0c61538e2ed7f110b53
Barbea <113396124+Barbea@users.noreply.github.com>
2022-12-21 11:34:34 +0000

    Update ServicesPublics.java

--- ProjetJava/src/consoCarbone/ServicesPublics.java
+++ ProjetJava/src/consoCarbone/ServicesPublics.java
@@ -1,13 +1,29 @@
 package consoCarbone;
-
+/**
+ * Cette classe permet de calculer l'impact d’émissions de GES en TCO2eq 
+ * lié aux services publics (justice, police, éducation, santé, ...)
+ * Tout les francais ont le même impact lié aux services publics (1.5TCO2eq)
+ *
+ */
 
 public class ServicesPublics  extends ConsoCarbone{
 	private double impact = 1.5;
+	/**
+         * objectif de consommation carbone lié aux services publiques (en TCO2eq)permettant 
+         * de limiter nos emissions de CO2
+         */
 	static double objectif = 1;
 	
+	/**
+	 * constructeur vide 
+	 */
 	public ServicesPublics() {
 	}
 	
+	/**
+	 * 
+	 * @return impact impact(en tonnes de gaz à effet de serre CO2) lié aux services publics 
+	 */
 	public double getimpact() {
 		return impact;
 	}
