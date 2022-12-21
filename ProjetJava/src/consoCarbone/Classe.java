386a5b1e6a544225bd04e5def543052f5c526fdf
Barbea <113396124+Barbea@users.noreply.github.com>
2022-12-21 11:58:53 +0000

    Update Classe.java

--- ProjetJava/src/consoCarbone/Classe.java
+++ ProjetJava/src/consoCarbone/Classe.java
@@ -1,15 +1,25 @@
 package consoCarbone;
 
+/** Cette enumeration représente les différentes classes possibles en avion et 
+ * leurs emissions correspondantes en tonne de CO2eq .
+ */
 public enum Classe {
 	
 	Eco(1), Business(2), Premiere(4);
-
+        
+	/**
+	 * impactClasse correspond à l'impact en tonne de CO2eq de sa classe dans l'avion
+	 */
 	public double impactClasse;
 	
 	private Classe(double impactClasse) {
 		this.impactClasse = impactClasse;
 	}
 	
+	/**
+	 * 
+	 * @return impactClasse l'impact en tonne de CO2eq de sa classe dans l'avion
+	 */
 	public double getImpactClasse() {
 		return impactClasse;
 	}
