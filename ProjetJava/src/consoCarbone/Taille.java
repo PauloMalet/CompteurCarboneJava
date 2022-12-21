e3b16acb1253142ec0b763cbc3cf83ddad1a2c1b
Barbea <113396124+Barbea@users.noreply.github.com>
2022-12-21 11:37:17 +0000

    Update Taille.java

--- ProjetJava/src/consoCarbone/Taille.java
+++ ProjetJava/src/consoCarbone/Taille.java
@@ -1,14 +1,24 @@
 package consoCarbone;
+/** Cette enumeration représente les différentes tailles possibles d’une voiture et 
+ * leurs emissions en tonne de CO2eq .
+ */
 
 public enum Taille {
 	P(4.2), G(19);
-
+	
+        /**
+	 * impact en TCO2eq de la production de la voiture de l'utilisateur
+	 */
 	public double impactTaille;
 	
 	private Taille(double impactTaille) {
 		this.impactTaille = impactTaille;
 	}
 	
+	/**
+	 * 
+	 * @return impactTaille impact de la production en TCO2eq de la voiture de l'utilisateur
+	 */
 	public double getImpactTaille() {
 		return impactTaille;
 	}
